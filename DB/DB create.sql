ef99d4f7478144c1e9644389c5b5ca2b2f8460af
atayarova <r.atayarova@gmail.com>
2023-01-29 20:49:09 +0600

    update

---
+++ DB/DB create.sql
@@ -0,0 +1,13 @@
+﻿CREATE DATABASE [Reports]
+ON  PRIMARY 
+( NAME = Reports, FILENAME = N'D:\Rosedocs\OTUS\Database\Reports.mdf' , 
+	SIZE = 8MB , 
+	MAXSIZE = UNLIMITED, 
+	FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = Reports_log, FILENAME = N'D:\Rosedocs\OTUS\Database\Reports_log.ldf' , 
+	SIZE = 8MB , 
+	MAXSIZE = 10GB , 
+	FILEGROWTH = 65536KB )
+GO
+---------------------------------------------------------------------------------
